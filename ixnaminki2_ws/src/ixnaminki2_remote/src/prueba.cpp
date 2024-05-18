329b24bfdd2e1a914c52c344c8948cd4045cb447
Chyaru <82061637+Chyaru@users.noreply.github.com>
2024-05-18 13:42:09 -0600

    Update prueba.cpp

--- ixnaminki2_ws/src/ixnaminki2_remote/src/prueba.cpp
+++ ixnaminki2_ws/src/ixnaminki2_remote/src/prueba.cpp
@@ -2,7 +2,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "control_msgs/msg/dynamic_joint_state.hpp"
-//#include "control_msgs/msg/interface_value.hpp"
+#include "formatos/srv/movearm.hpp"
 
 using std::placeholders::_1;
 
@@ -19,6 +19,7 @@ class MinimalSubscriber : public rclcpp::Node
       subscription_ = this->create_subscription<control_msgs::msg::DynamicJointState>(
       "dynamic_joint_states", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
       last_joints.resize(6, 0);
+      clientCambioEjes = node->create_client<formatos::srv::Movearm>("cambio_ejes");
     }
 
   private:
@@ -49,6 +50,20 @@ class MinimalSubscriber : public rclcpp::Node
             RCLCPP_INFO(this->get_logger(), "Posicion final: [%.2f] [%.2f] [%.2f] [%.2f] [%.2f] [%.2f]", 
                 msg->interface_values[0].values[0],msg->interface_values[1].values[0], msg->interface_values[2].values[0], msg->interface_values[3].values[0],
                 msg->interface_values[4].values[0], msg->interface_values[5].values[0]);
+
+                auto request = std::make_shared<formatos::srv::Movearm::Request>();
+
+                for(int i = 0; i < 6; i++){
+                    request->armi[i]=msg->interface_values[i].values[0];
+                }
+                auto result = clientCambioEjes->async_send_request(request);
+                if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS){
+                    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "succes: %d", true);
+                }else{
+                    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service direcciones");
+                }
+
+            
         }
         last_joints = joints;
 
@@ -56,6 +71,7 @@ class MinimalSubscriber : public rclcpp::Node
       //  msg->interface_values[0].values,msg->interface_values[1].values, msg->interface_values[2].values, msg->interface_values[3].values,
       //  msg->interface_values[4].values, msg->interface_values[5].values);
     }
+    rclcpp::Client<formatos::srv::Movearm>::SharedPtr clientCambioEjes;
     rclcpp::Subscription<control_msgs::msg::DynamicJointState>::SharedPtr subscription_;
 };
 
